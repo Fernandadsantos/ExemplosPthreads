ed8872eead45c8931c950c4b93b6c1b0cc7d942a
Ubuntu <ubuntu@ip-172-31-67-40.ec2.internal>
2025-01-17 21:06:09 +0000

    aula 4

--- pth_hello1.c
+++ pth_hello1.c
@@ -4,25 +4,46 @@
 #include <unistd.h>
 
 void *Hello(void* param);  /* Thread function */
-
+void *Hello2(void* param);  /* Thread function */
 /*--------------------------------------------------------------------*/
 int main(int argc, char* argv[]) {
-   long t1_param = 1;
-   pthread_t t1;
+   long t_param = 10;
+   pthread_t t1, t2;
+    
    
-   printf("Hello da thread principal!\n");
+   pthread_create(&t1, NULL, Hello, (void*) t_param);  
+   pthread_create(&t2, NULL, Hello2, (void*) t_param);
    
-   pthread_create(&t1, NULL, Hello, (void*) t1_param);  
-   pthread_join(t1, NULL); 
+   for(int i = 0;i < 5; i++){
+      printf("%dM\n", i);
+      sleep(1);
+   }
 
+   pthread_join(t1, NULL); //espera a thread terminar
+   pthread_join(t2, NULL); //espera a thread terminar
    return 0;
 }  /* main */
 
 /*-------------------------------------------------------------------*/
-void *Hello(void* param) {
-   long my_id = (long) param;  /* Use long in case of 64-bit system */ 
-
-   printf("Hello da thread %ld\n", my_id);
+void *Hello(void* param) { 
+   long my_id = (long)param;
+  
+   for(int i = 0;i < my_id; i++){
+      printf("%d Thread 1\n", i);
+      sleep(1);
+   }
+   
    return NULL;
 }  /* Hello */
 
+void *Hello2(void* param) {  
+   
+   long my_id = (long)param;
+   
+   for(int i = 0;i < my_id; i++){
+      printf("%d  Thread 2\n", i);
+      sleep(1);
+   }
+   
+   return NULL;
+}  /* Hello */
